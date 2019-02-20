07b3431073c54a02d4212fd5ac6a3d64ddfcc2ff
Anton Dvornikov <anton@MacBook-Pro-Anton.local>
2019-02-20 20:49:27 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (matrix[row][col] === 0) {
+        let impossible = []
+        for (let index = 0; index < 9; index++) {
+          impossible.push(matrix[row][index])
+          impossible.push(matrix[index][col])
+        }
+        const startRow = Math.floor(row / 3) * 3
+        const startCol = Math.floor(col / 3) * 3
+        for (qRow = startRow; qRow < startRow + 3; qRow++) {
+          for (qCol = startCol; qCol < startCol + 3; qCol++) {
+            impossible.push(matrix[qRow][qCol])
+          }
+        }
+        const possibles = [1, 2, 3, 4, 5, 6, 7, 8, 9].filter(digit => impossible.indexOf(digit) < 0)
+        for (let possible = 0; possible < possibles.length; possible++) {
+          matrix[row][col] = possibles[possible]
+          const solve = solveSudoku(matrix)
+          if (solve) return solve
+        }
+        matrix[row][col] = 0
+        return false
+      }
+    }
+  }
+  return matrix
 }
